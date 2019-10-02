bee49a777eae356348ba587fa6e50396e8176e58
Papa thiam <papathiame11@gmail.com>
2019-10-02 00:01:15 +0000

    update page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html class="loading" lang="fr" data-textdirection="ltr" ng-app="appModule">
 
+<body ng-controller="BackEndCtl">
 @include('layouts.partials.header')
 
 @yield('content')
 
 @include('layouts.partials.footer')
+</body>
 
 </html>
--- resources/views/pages/liste_inscriptions.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: khazimndiaye
- * Date: 8/30/19
- * Time: 12:50 AM
- */
